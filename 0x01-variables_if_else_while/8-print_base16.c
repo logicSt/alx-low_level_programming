b8b8ba9a1ab94bbd49efd390737e8e109f1ef6a3
logicSt <olumide.f.akinduko@gmail.com>
2021-06-22 08:12:16 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+char hexa;
+
+for (hexa = '0'; hexa <= '9'; hexa++)
+{
+putchar(hexa);
+}
+
+for (hexa = 'a'; hexa <= 'f'; hexa++)
+{
+putchar(hexa);
+}
+
+putchar('\n');
+
+return (0);
+
+}
