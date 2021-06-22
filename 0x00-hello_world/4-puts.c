6eb604167029b0da19ca0f33ebfab91db9d897dd
logicSt <olumide.f.akinduko@gmail.com>
2021-06-21 17:05:13 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+puts("Programming is like a building a multilingual puxxle");
+
+return (0);
+
+}
