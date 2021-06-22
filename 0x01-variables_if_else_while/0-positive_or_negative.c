eb18e057ac0e0b269976358cac6a864150454a70
logicSt <olumide.f.akinduko@gmail.com>
2021-06-22 05:12:36 -0700

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,11 +18,11 @@ n = rand() - RAND_MAX / 2;
 
 if (n < 0)
 {
-printf("%d is negative \n", n);
+printf("%d is negative\n", n);
 }
 else if (n > 0)
 {
-printf("%d is positive \n", n);
+printf("%d is positive\n", n);
 }
 else
 {
