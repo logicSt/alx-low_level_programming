6ee737149972b3909cf11d3beafa6b93dedccd86
logicSt <olumide.f.akinduko@gmail.com>
2021-06-22 07:05:12 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+char alpha;
+
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+{
+putchar(alpha);
+}
+
+for (alpha = 'A'; alpha <= 'Z'; alpha++)
+{
+putchar(alpha);
+}
+
+putchar('\n');
+
+return (0);
+
+}
