cad48bea93b66696c44a6a6d41c14c206ee35b8c
logicSt <olumide.f.akinduko@gmail.com>
2021-06-23 14:04:12 -0700

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main- prints all fibanaccie nums
+ *       starting with the second 1
+ *
+ * Return: alwayse 0
+ *
+ * does- not excede 4000000
+ */
+int main(void)
+{
+long int a, b, c, d;
+a = 0;
+b = 1;
+c = 0;
+d = 0;
+while (1)
+{
+c = a + b;
+a = b;
+b = c;
+if (c > 4000000)
+break;
+if ((c % 2) == 0)
+d += c;
+}
+printf("%ld\n", d);
+return (0);
+}
