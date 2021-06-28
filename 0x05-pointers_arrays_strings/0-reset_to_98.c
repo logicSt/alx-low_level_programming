a15d2f6e7aa68bb47222c754731473985561bc4e
logicSt <olumide.f.akinduko@gmail.com>
2021-06-28 08:44:13 -0700

    0. 98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter and updates the value
+ * @n: pointer to int n
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
